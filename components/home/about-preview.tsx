a81b4dbe4787272b2703206cf348d58a1f50aaa9
MetabonDTech <122605801+MetabonDTech@users.noreply.github.com>
2025-11-12 10:31:47 +0100

    Save

--- components/home/about-preview.tsx
+++ components/home/about-preview.tsx
@@ -66,7 +66,7 @@ export function AboutPreview() {
 							<Card className="card-gradient">
 								<CardContent className="p-4">
 									<h4 className="font-semibold">Projects</h4>
-									<p className="text-sm text-muted-foreground">104+ Completed</p>
+									<p className="text-sm text-muted-foreground">54+ Completed</p>
 								</CardContent>
 							</Card>
 							<Card className="card-gradient">
