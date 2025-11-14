b6f712c17d82a6143772ea87f68154481a36ff04
MetabonDTech <122605801+MetabonDTech@users.noreply.github.com>
2025-11-14 15:41:47 +0100

    test

--- lib/constants.ts
+++ lib/constants.ts
@@ -121,7 +121,7 @@ export const projects: Project[] = [
 		title: 'PropTech System',
 		description:
 			'AI-powered platform resolving agent-tenant challenges with GIS and smart property matching.',
-		image: '/images/myapati.jpg',
+		image: 'public/images/myapati.jpg',
 		tags: ['php', 'Ajax', 'AI', 'Mobile App'],
 		link: '#',
 		repo: '#',
@@ -130,7 +130,7 @@ export const projects: Project[] = [
 		title: 'Lottery Website (Iplanet):',
 		description:
 			'Designed and developed a dynamic lottery management system with secure payment integration and live draws.',
-		image: '/images/Iplanet.jpg',
+		image: 'public/images/Iplanet.jpg',
 		tags: ['Typescript', 'React.Js', 'APIs', 'Node.Js'],
 		repo: '#',
 	},
@@ -147,7 +147,7 @@ export const projects: Project[] = [
 		title: 'Sune-x Contruction Company Portfolio',
 		description:
 			'Developed a user-friendly website for Sune-x Constructions, showcasing their services, past projects,and providing easy contact options for clients.',
-		image: '/images/Sune-x.png',
+		image: 'public/images/Sune-x.png',
 		tags: ['HTML5', 'CSS3', 'AJAX', 'PHP'],
 		link: 'https://www.sunexcontruction.site',
 		repo: '#',
